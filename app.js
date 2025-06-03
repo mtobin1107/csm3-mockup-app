b724860209d399908f5512e47e27a52bc7b01e61
mtobin1107 <github@wirelesseco.com>
2025-06-02 21:32:33 -0700

    add bootstrap

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
+// app.js
 const express = require('express');
 const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-// Serve static files
 app.use(express.static(__dirname));
 
-// Fallback route (optional)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Mockup app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
